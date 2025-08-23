e30503f929fa67495bb359c4d8c68c1f941aa44e
Alby <albymbiju7@gmail.com>
2025-08-23 22:47:51 +0530

    Imports Logout and registers route alongside /, /login, /register

--- smartjewel-frontend/src/router.tsx
+++ smartjewel-frontend/src/router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { App } from './pages/App';
 import { LandingPage } from './pages/LandingPage';
+import { Logout } from './pages/Logout';
 
 export const AppRouter: React.FC = () => (
   <Router>
@@ -9,6 +10,7 @@ export const AppRouter: React.FC = () => (
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<App />} />
       <Route path="/register" element={<App />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   </Router>
 );

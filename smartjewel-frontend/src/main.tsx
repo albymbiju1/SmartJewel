5260d197889c3ec71310a07af2f0abb1f83b010f
Alby <albymbiju7@gmail.com>
2025-08-23 22:44:32 +0530

    Replaces ./styles.css with ./index.css to use consolidated styles

--- smartjewel-frontend/src/main.tsx
+++ smartjewel-frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './router';
 import { AuthProvider } from './contexts/AuthContext';
-import './styles.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
